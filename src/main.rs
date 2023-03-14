8996a9ea92a010a5ccc7e26e4a8102523845364c
spiegel-jpg <ds76520@uga.edu>
2023-03-14 10:18:44 -0400

    Updated code from Jacks branch. Added corpus for different languages.

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,91 @@
 use rand::{Rng, thread_rng};
+use std::fs;
+
+enum Corpus<'a> {
+    FromFile(&'a str), 
+}
 
 fn main() {
     let mut rng = thread_rng();
-    let language = rng.gen_range(0..3);
+    let language = rng.gen_range(0..8);
     let paragraph = match language {
-        0 => generate_paragraph("english"),
-        1 => generate_paragraph("french"),
-        2 => generate_paragraph("spanish"),
+        0 => generate_paragraph(Corpus::FromFile("english.txt"), Some(100), Some(1000)),
+        1 => generate_paragraph(Corpus::FromFile("french.txt"), None, None),
+        2 => generate_paragraph(Corpus::FromFile("spanish.txt"), None, Some(500)),
+        3 => generate_paragraph(Corpus::FromFile("hindi.txt"), Some(50), None),
+        4 => generate_paragraph(Corpus::FromFile("russian.txt"), Some(50), None),
+        5 => generate_paragraph(Corpus::FromFile("arabic.txt"), Some(50), None),
+        6 => generate_paragraph(Corpus::FromFile("japanese.txt"), Some(50), None),
+        7 => generate_paragraph(Corpus::FromFile("german.txt"), Some(50), None),
         _ => panic!("Invalid language index"),
     };
     println!("{}", paragraph);
 }
 
-fn generate_paragraph(language: &str) -> String {
+fn read_corpus_from_file(path: &str) -> Vec<String> {
+    fs::read_to_string(path)
+        .expect("Failed to parse file.")
+        .split("\n")
+        .map(|s| s.to_owned())
+        .collect::<Vec<String>>()
+}
+
+fn generate_paragraph(corpus: Corpus, min_sentences: Option<usize>, max_bytes: Option<usize>) -> String {
     let mut rng = thread_rng();
-    let word_list = match language {
-        "english" => vec![
-            "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog",
-            "a", "man", "walks", "into", "a", "bar", "the", "bartender", "says", "what'll", "it", "be",
-            "to", "be", "or", "not", "to", "be", "that", "is", "the", "question",
-            "there", "are", "many", "variations", "of", "passages", "of", "Lorem", "Ipsum", "available",
-            "but", "the", "majority", "have", "suffered", "alteration", "in", "some", "form", "or", "another",
-        ],
-        "french" => vec![
-            "le", "chat", "est", "sur", "le", "toit",
-            "je", "suis", "fatigué", "aujourd'hui",
-            "une", "fois", "n'est", "pas", "coutume",
-            "la", "vie", "est", "belle", "et", "pleine", "de", "surprises",
-            "il", "y", "a", "toujours", "quelque", "chose", "à", "apprendre",
-        ],
-        "spanish" => vec![
-            "el", "gato", "está", "en", "el", "tejado",
-            "hoy", "es", "un", "día", "soleado",
-            "a", "veces", "la", "vida", "es", "difícil",
-            "no", "hay", "mal", "que", "por", "bien", "no", "venga",
-            "lo", "importante", "es", "mantenerse", "siempre", "positivo",
-        ],
-        _ => panic!("Invalid language"),
+
+    let word_list: Vec<String> = match corpus {
+        Corpus::FromFile(f) => {
+            read_corpus_from_file(f)
+        }, 
     };
-    let n_sentences = rng.gen_range(3..7);
+
+    let n_sentences;
+    if let Some(n) = min_sentences {
+        n_sentences = rng.gen_range(n..n+4);
+    } else {
+        n_sentences = rng.gen_range(3..7);
+    }
+
     let mut sentences = Vec::new();
-    let mut sentence_start = true;
     for _ in 0..n_sentences {
         let n_words = rng.gen_range(5..12);
         let mut words = Vec::new();
-        let mut use_conjunction = !sentence_start;
         for _ in 0..n_words {
             let word_index = rng.gen_range(0..word_list.len());
-            let word = word_list[word_index];
-            if word == "," || word == ":" || word == ";" || word == "?" || word == "!" {
-                use_conjunction = false;
-            } else if use_conjunction {
-                words.push(",");
-                words.push(word);
-                use_conjunction = false;
-            } else {
-                words.push(word);
-                use_conjunction = true;
-            }
+            let word = word_list[word_index].as_str();
+            words.push(word);
         }
-        sentence_start = false;
+
         let mut sentence = words.join(" ");
         sentence.make_ascii_lowercase();
+
         let first_char = sentence.chars().next().unwrap();
-        sentence.replace_range(..1, &first_char.to_uppercase().to_string());
-        if let Some(last_char) = sentence.chars().rev().next() {
-            if last_char != '?' && last_char != '!' {
-                sentence.push('.');
-            }
-        }
+        //first character may not be one byte-aligned
+        let first_char_len = &first_char.len_utf8();
+        sentence.replace_range(..first_char_len, &first_char.to_uppercase().to_string());
+        sentence.push('.');
         sentences.push(sentence);
     }
-    sentences.join(" ")
+    
+    let paragraph = sentences.join(" ");
+    
+    match max_bytes {
+        None => paragraph, 
+        Some(m) => {
+            //only read up to limit of bytes requested by the user
+            let mut truncated = String::new();
+            let mut num_bytes: usize = 0;
+            for char in paragraph.chars() {
+                let len = char.len_utf8();
+                if num_bytes + len < m {
+                    truncated.push(char);
+                    num_bytes += len;
+                } else {
+                    break;
+                }
+            }
+
+            truncated
+        }
+    }
 }
