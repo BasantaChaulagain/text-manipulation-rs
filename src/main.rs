5b264b017e807640dad34bebeb97691764c487a7
Basanta Chaulagain <basanta83chaulagain@gmail.com>
2023-04-07 13:39:43 -0400

    comments added

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,18 @@ mod my_memory;
 use my_memory::translate_q_langpair;
 
 fn main(){
+    // code to test random text generator code.
     let mut rng = thread_rng();
     let language = rng.gen_range(0..8);
     generate_text_for_language(language, false);
 
-
+    // code to test sentence translation code.
     let q = String::from("My name is Aaron"); 
     let langpair = String::from("en|hi"); 
     let translated_text = translate_q_langpair(q, langpair);
     println!("{}", translated_text);
-}
 
-
-
-// fn main() {
+    // code to test word definition code.
     
-//     
-
-// }
-
-
+}
 
