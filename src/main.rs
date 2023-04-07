0ee51628cb5ae01a312f233f94706309480a653b
Jack Bass <jtb01584@uga.edu>
2023-04-07 11:59:46 -0400

    Converted DeepL API to modules for library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use rand::{Rng, thread_rng};
+use text_manipulation_rs::{read_secret, DeepLKey};
 use std::{fs, error::Error, io::{Write}, time::{SystemTime, UNIX_EPOCH}};
+
+use text_manipulation_rs::request::translation_request::TranslationRequest;
+use text_manipulation_rs::request::glossary_request::{get_glossaries, get_glossary, delete_glossary, create_glossary_from_string};
+use text_manipulation_rs::deepl::{SourceLang, TargetLang};
 
 enum Corpus<'a> {
     FromFile(&'a str), 
 }
 fn main(){
-    let mut rng = thread_rng();
-    let language = rng.gen_range(0..8);
-    generate_text_for_language(language, false);
+    // let mut rng = thread_rng();
+    // let language = rng.gen_range(0..8);
+    // generate_text_for_language(language, false);
+
+    deepl();
 }
 
 fn generate_text_for_language(language: i32, write_to_file: bool) {
@@ -118,4 +125,70 @@ fn generate_paragraph(corpus: Corpus, min_sentences: Option<usize>, max_bytes: O
             truncated
         }
     }
+}
+
+fn deepl() {
+    let secret = read_secret("secret.txt");
+    let auth = DeepLKey{key: secret};
+
+    //translation
+
+    // let text = ["Philosophy is based on a false pretense, so philosophy itself is nonsense."];
+
+    // let tr = TranslationRequest::new(Box::new(text), TargetLang::De);
+    // let request = TranslationRequest::create_request(&tr, &auth);
+
+    // let res = request.execute();
+    // match res {
+    //     Ok(v) => println!("OK: {}", v), 
+    //     Err(_) => println!("Flop")
+    // };
+
+    //glossary fun
+
+    let glossary_request = get_glossaries(&auth);
+
+    if let Ok(glossaries) = glossary_request {
+        for glossary in glossaries {
+            println!("NEW GLOSSARY: \n{}\n", glossary);
+        }
+    } else {
+        println!("UH OH");
+    }
+
+    // get specific glossary
+    let gid = "36ee07b0-ddc9-426e-a273-57b1dba75291".to_string();
+    let glossary_request = get_glossary(&auth, gid);
+
+    if let Ok(gloss) = glossary_request {
+        println!("\n\n\nGLOSS: {}", gloss);
+    } else {
+        println!("Error getting glossary");
+    }
+
+    //delete specific glossary
+    let gid = "2d55f35e-2b81-47d9-a12b-89498e540874".to_string();
+    let delete_request = delete_glossary(&auth, gid);
+    
+    if let Ok(()) = delete_request {
+        println!("YIPEEEE!");
+    } else {
+        println!("Failed to delete glossary");
+    }
+
+    //create a glossary
+    // let glossary = String::from("Hello\tGabagool\nBye\tVa Fangul");
+    // let create_request = create_glossary_from_string(&auth, "Nonsense".to_string(), SourceLang::En, TargetLang::It, glossary);
+
+    // if let Ok(g) = create_request {
+    //     println!("glossary_id: {}", g["glossary_id"]);
+    //     println!("ready: {}", g["ready"]);
+    //     println!("name: {}", g["name"]);
+    //     println!("source_lang: {}", g["source_lang"]);
+    //     println!("target_lang: {}", g["target_lang"]);
+    //     println!("creation_time: {}", g["creation_time"]);
+    //     println!("entry_count: {}", g["entry_count"]);
+    // } else {
+    //     println!("Failed to create glossary");
+    // }
 }
---
+++ src/request/glossary_request.rs
@@ -0,0 +1,142 @@
+use std::collections::HashMap;
+
+use crate::deepl::{SourceLang, TargetLang, Glossary};
+use crate::DeepLKey;
+use crate::request::http_request::{HttpRequest, RequestType};
+use serde_json::Value;
+
+pub fn create_glossary_from_string(auth: &DeepLKey, name: String, source_lang: SourceLang, target_lang: TargetLang, entries: String) -> Result<Value, Box<dyn std::error::Error>> {
+    let endpoint = "https://api-free.deepl.com/v2/glossaries";
+    let mut params : Vec<String> = Vec::new();
+
+    params.push(format!("name={}", name));
+    params.push(format!("source_lang={}", source_lang));
+    params.push(format!("target_lang={}", target_lang));
+    params.push(format!("entries={}", entries));
+    params.push("entries_format=tsv".to_string());
+
+    let request = HttpRequest {
+        endpoint: endpoint, 
+        auth: &auth.key, 
+        headers: None, 
+        body: Some(params), 
+        request_type: RequestType::Post, 
+    };
+
+    let res = request.execute();
+
+    if let Ok(v) = res {
+        Ok(v)
+    } else {
+        panic!("I forgot how to pass error statements")
+    }
+}
+
+pub fn get_glossaries(auth: &DeepLKey) -> Result<Vec<Glossary>, Box<dyn std::error::Error>> {
+    let endpoint = "https://api-free.deepl.com/v2/glossaries";
+    let mut glossaries : Vec<Glossary> = Vec::new();
+
+    let request = HttpRequest {
+        endpoint: endpoint,
+        auth: &auth.key,
+        headers: None, 
+        body: None, 
+        request_type: RequestType::Get
+    };
+
+    let res = request.execute();
+
+    if let Ok(v) = res {
+        let g = &v["glossaries"];
+        let arr = g.as_array().unwrap();
+
+        for entry in arr.to_owned() {
+            let gloss = Glossary::new(entry);
+            glossaries.push(gloss);
+        }
+
+        Ok(glossaries)
+    } else {
+        panic!("I forgot how to pass error statements")
+    }
+}
+
+pub fn get_glossary(auth: &DeepLKey, glossary_id: String) -> Result<Glossary, Box<dyn std::error::Error>> {
+    let endpoint = format!("https://api-free.deepl.com/v2/glossaries/{}", glossary_id);
+
+    let request = HttpRequest {
+        auth: &auth.key, 
+        endpoint: &endpoint.as_str(), 
+        headers: None, 
+        body: None, 
+        request_type: RequestType::Get
+    };
+
+    let res = request.execute();
+
+    if let Ok(g) = res {
+        let glossary = Glossary::new(g);
+
+        Ok(glossary)
+    } else {
+        panic!("Git gud");
+    }
+}
+
+pub fn delete_glossary(auth: &DeepLKey, glossary_id: String) -> Result<(), Box<dyn std::error::Error>> {
+    let endpoint = format!("https://api-free.deepl.com/v2/glossaries/{}", glossary_id);
+
+    let request = HttpRequest {
+        endpoint: endpoint.as_str(), 
+        auth: &auth.key, 
+        headers: None, 
+        body: None, 
+        request_type: RequestType::Delete
+    };
+
+    let res = request.execute();
+
+    if let Ok(m) = res {
+        println!("MESSAGE: {}", m);
+
+        //TODO: return error if message "Not found" returned
+
+        Ok(())
+    } else {
+        panic!("Git gud");
+    }
+}
+
+pub fn get_glossary_entries(auth: &DeepLKey, glossary_id: String) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+    let endpoint = format!("https://api-free.deepl.com/v2/glossaries/{}/entries", glossary_id);
+
+    // let params = vec![];
+    let mut params : Vec<String> = Vec::new();
+    params.push(String::from("Accept: text/tab-separated-values"));
+
+    let request = HttpRequest {
+        endpoint: endpoint.as_str(), 
+        auth: &auth.key, 
+        headers: Some(params), 
+        body: None, 
+        request_type: RequestType::Get
+    };
+
+    let res = request.execute();
+
+    if let Ok(map) = res {
+        let mut hm : HashMap<String, String> = HashMap::new();
+
+        //TODO: FIX
+        // let rows : Vec<&str> = map.split("\n").collect();
+        // for row in rows {
+        //     let key_val : Vec<&str> = row.split("\t").collect();
+
+        //     hm.insert(key_val[0].to_string(), key_val[1].to_string());
+        // }
+
+        Ok(hm)
+    } else {
+        panic!("Git gud");
+    }
+}
